e29813270fe8095c0c5062fdc21ce60a1f03fab3
CaraAkame <136689399+CaraAkame@users.noreply.github.com>
2023-07-14 18:36:02 +0200

    Update NonogramClueGeneratorV1.py

--- NonogramClueGenerator.py
+++ NonogramClueGenerator.py
@@ -1,30 +1,33 @@
 """
 Nonogramm Clue Creator
-(https://www.conceptispuzzles.com/index.aspx?uri=puzzle/pic-a-pix)
-Nonogramm aus .txt file oder von Programm lesen
-warn if more than 2 char types
-Calculate horizontal clues
-Calculate vertical clues
-For use in GUI
+- where I get nonograms: (https://www.conceptispuzzles.com/index.aspx?uri=puzzle/pic-a-pix)
+- Warn if more than 2 char types
+- Read nonogram from .txt file or NonoMakerGUI
+- Calculate horizontal clues
+- Calculate vertical clues
+- Give back to GUI
+
+Notes:
+some notes were for me, this was my first program on Python
 """
 
 import os
 import os.path
 
-allowedfileextensions = [".txt",".csv",".log",".ini",".nono"]
+allowedfileextensions = [".txt",".csv",".log",".ini",".nono"] 
 
 logfilename = "output.csv"
 loglist = ["Filename,Width,Height,\n"]
 
-startdir = os.getcwd()                                  # find current working directory
+startdir = os.getcwd()                                      # find current working directory
 
-c_y_sep = "*"                                           # separation char for y clues
+c_y_sep = "*"                                               # separation char for y clues
 c_x_sep = "*"
 
 # function paint
 def paintnono(array,c_x,c_y,black="#",white=" "):
     print("+"+len(array[0])*"-"+"+")
-    for y,row in enumerate(array):                      # enumerate = gibt jedem Element(row) in einer Liste einen Index (y)
+    for y,row in enumerate(array):                          # enumerate = gives each element(row) in a list an index (y)
         print("|", end="")
         for number in row:
             if number == 0:
@@ -35,7 +38,7 @@ def paintnono(array,c_x,c_y,black="#",white=" "):
         print("| ", end="")
         print(str(c_x[y]).replace(", ",c_x_sep))           # show calc values
     print("+"+len(array[0])*"-"+"+")
-    # find longest result array (mehrstellige Zahlen!) in c_y
+    # find longest result array (numbers with multiple digits!) in c_y
     max_y = 0
     for row in c_y:
         y = 0
@@ -44,7 +47,7 @@ def paintnono(array,c_x,c_y,black="#",white=" "):
             y += 1
         if y > max_y:
             max_y = y
-    t_c_y = []                                            # gestürzte c_y (transformed c_y)
+    t_c_y = []                                              # transposed c_y
     for y in range (max_y):
         row = []
         for x in range (len(array[0])):
@@ -59,7 +62,7 @@ def paintnono(array,c_x,c_y,black="#",white=" "):
                 # seperation char
                 t_c_y[y][x] = c_y_sep
                 y += 1
-            for digit in str(number).strip():                    # digit = Stelle
+            for digit in str(number).strip():               
                 t_c_y[y][x] = digit
                 y += 1            
 
@@ -70,19 +73,19 @@ def paintnono(array,c_x,c_y,black="#",white=" "):
 # function file to array
 def file_to_array(filename):
     # read file
-    with open(filename) as f:                           # if indented block ends, file is automatically closed
+    with open(filename) as f:                               # if indented block ends, file is automatically closed
         lines = f.readlines()
 
     # cleanup excess (linebreaks)
-    cleanlines = [list(line.strip()) for line in lines] # .strip nur für string!
+    cleanlines = [list(line.strip()) for line in lines]     # .strip only for string!
     return cleanlines
 
 # function generate clues
 def gen_clues(cleanlines):
 
     array = []
-    c_x = []                                            # clues for rows in array
-    c_y = []                                            # clues for columns in array
+    c_x = []                                                # clues for rows in array
+    c_y = []                                                # clues for columns in array
     charlist = []
 
     # replace with 0 & 1
@@ -104,7 +107,7 @@ def gen_clues(cleanlines):
             WHITE = charlist[0]
             BLACK = None
         else:
-            WHITE,BLACK = charlist                  # same as above but better
+            WHITE,BLACK = charlist                          # same as above but better
     else:
         if len(charlist) == 1:
             BLACK = charlist[0]
@@ -128,32 +131,32 @@ def gen_clues(cleanlines):
     for row in array:
         filled = False                          
         result = []
-        blocks = 0                                      # Länge des vektors wenn filled = True --> reset values for new row
-        for number in row:                              # for each element (number) in the row
+        blocks = 0                                          # length of vactor when filled = True --> reset values for new row
+        for number in row:                                  # = for each element (number) in the row
             if number == 0:
                 filled = False
             else:
                 filled = True
             if filled :
-                blocks = blocks +1                      # =blocks += 1  
+                blocks = blocks +1                          # = blocks += 1  
             elif blocks != 0:
-                result.append(blocks)                   # append blocks to the end of result (insert would be opposite)
-                blocks = 0                              # reset blocks variable
+                result.append(blocks)                       # append blocks to the end of result (insert would be opposite)
+                blocks = 0                                  # reset blocks variable
         # finish with row
-        result.append(blocks)                           # append even if it ends with "filled"
+        result.append(blocks)                               # append even if it ends with "filled"
         # we don't accept trailing 0s in c_x, but we accept lone 0s
         if len(result) > 1:
             if result[-1] == 0:
                 result = result[:-1]
-        c_x.append(result)                              # new horizontal clues at the end
+        c_x.append(result)                                  # new horizontal clues at the end
     
     #calculating columns
-    for x in range(len(array[0])):                      # len = length of array
+    for x in range(len(array[0])):                          # len = length of array
         filled = False
         result = []
         blocks = 0
-        for y in range(len(array)):                     # len(array) = stopp wert hier
-            number = array[y][x]                        # Wollen die Zeile zuerst wissen, dann position
+        for y in range(len(array)):                         # len(array) = stop value here
+            number = array[y][x]                            # we want the row first, then the position
             if number == 0:
                 filled = False
             else:
@@ -161,7 +164,7 @@ def gen_clues(cleanlines):
             if filled :
                 blocks = blocks +1              
             elif blocks != 0:
-                result.append(blocks)                   # append blocks to the end of result
+                result.append(blocks)                       # append blocks to the end of result
                 blocks = 0
         # finish with column
         result.append(blocks)
@@ -173,8 +176,7 @@ def gen_clues(cleanlines):
     return c_x,c_y
 
 def main():
-    # https://docs.python.org/3/library/os.html?highlight=os%20walk#os.walk
-    for root, dirs, files in os.walk(startdir):             # "." = current dir ("C:\" wäre dann die ganze C Festplatte)
+    for root, dirs, files in os.walk(startdir):             # "." = current dir ("C:\" would be the entire harddrive)
         for filename in files:
             validfilename = False
             for extension in allowedfileextensions:
@@ -187,5 +189,5 @@ def main():
                     continue                        
 
 # iterations, body
-if __name__ == "__main__":                                  # nur machen wenn aufgerufen wird
+if __name__ == "__main__":                                  # only do this if it's called
     main()
